4108ddb8bee259c353c97659f1de1646b0efcada
dashing-utility-e4d8 <eten.uket@yale.edu>
2021-06-08 16:06:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 def hopper
 	programmer_hash = 
  		{
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,6 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -52,22 +53,17 @@ def dennis_ritchies_language
       },
       :dennis_ritchie => {
         :known_for => "Unix",
-        :languages => ["C"]
+        :languages => "C"
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages]
 end
 
-def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]
-# }
-# return the entire updated hash
 
+def adding_matz
+  
 	programmer_hash = 
- 		{
+ 		  {
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -82,8 +78,19 @@ def adding_matz
       }
     }
 
-end
 
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+puts programmer_hash
+
+
+#rogrammer_hash[:yukihiro_matsumoto] = {}
+#programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+
+
+#yukihiro_info = [:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]}]
+
+end
+binding.pry
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
   # return the entire updated hash
@@ -103,8 +110,11 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+   changing_alan[programmer_hash][0]
+   changing_alan[programmer_hash][1]
 end
+  
+
 
 def adding_to_dennis
   # add "Assembly" to Dennis Ritchie's languages array
